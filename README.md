492ff294937ba8ad9e9a6963a1827003044b96f0
Mo Valipour <mvalipour@neworbit.co.uk>
2015-06-29 22:57:48 +0100

    - Read project files instead of base path - Improve watch paths

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ A library that facilitates creating gulp tasks for asp.net dnx web apps.
 There are generally two types of tasks that can be created using this:
 
 - `test`: this will run all the test commands in all of your test projects with the option to watch files and re-run the tests for each project. (at the time of writing this, it is not possible to achieve this by `dnx` command line interface. See the issue here: [aspnet/dnx#2148](https://github.com/aspnet/dnx/issues/2148))
-- `web`: this will run the web command for your web project with the option to watch files and re-start the command.
+- `web`: this will run the web command for your web project (the only project with
+a `cmd` command) with the option to watch files and re-start the command.
 
 ## Usage
 
@@ -17,8 +18,8 @@ npm install gulp-dnx-tasks --save-dev
 var gulp = require('gulp');
 var dnx = require('gulp-dnx-tasks');
 
-dnx.webTasks(gulp, { base: './src/my-web-app' });
-dnx.testTasks(gulp);
+dnx.webTasks(gulp);
+   .testTasks(gulp);
 ```
 
 then you will have the following tasks defined:
@@ -35,12 +36,14 @@ $ gulp test-watch
 
 options:
 - `cmd` (defaults to `web`): dnx command to start the web server
-- `base` (mandatory! no default): relative path to the web project root. e.g. `./src/my-web-app`
-- `watch` (defaults tp `['/**/*.cs']`): files to watch on a watch task
+- `base` (defaults to the only project with web command): relative path
+to the web project root. e.g. `./src/my-web-app`
+- `watch` (defaults to `['./src/**/*.cs']`): files to watch on a watch task.
+    *Note:* You can use `{project}` in the path to refer to the root of the web project.
 
 #### .testTasks(< gulp >, < options >);
 
 options:
 - `cmd` (defaults to `test`): dnx command to run tests
-- `base` (defaults to `./test`): relative path to the directory that contains test projects.
-- `watch` (defaults tp `['/**/*.cs']`): files to watch on a watch task
+- `watch` (defaults tp `['{project}/**/*.cs']`): files to watch on a watch task.
+    *Note:* You can use `{project}` in the path to refer to the root of the web project.
--- src/index.js
+++ src/index.js
@@ -4,48 +4,75 @@ var fs = require('fs');
 var path = require('path');
 var spawn = require('child_process').spawn;
 var ext = require('deep-extend');
+var glob = require('glob');
 
 var defaults = {
     test: {
         cmd: 'test',
-        base: './test',
-        watch: ['/**/*.cs']
+        watch: ['{project}/**/*.cs']
     },
     web: {
         cmd: 'web',
-        watch: ['/**/*.cs']
+        watch: ['./src/**/*.cs']
     }
 };
+
+function getCommands() {
+    gutil.log('Reading configuration files...');
+    var files = glob.sync("./**/project.json", {});
+    gutil.log('Found', files.length, 'projects:');
+    var commands = {};
+    files.forEach(function (f) {
+        var config = JSON.parse(fs.readFileSync(f).toString().trim());
+        for(var c in config.commands) (commands[c] || (commands[c] = [])).push(f);
+    });
+    for(var c in commands) gutil.log('[' + c + ']', commands[c].length, 'projects.');
+
+    return commands;
+}
+
+var commands = getCommands();
+
+function processPath(path, dir) {
+    return path.replace('{project}', dir);
+}
 
 function initTest(gulp, opts) {
     opts = ext({}, opts, defaults.test);
 
-    var testDir = opts.base;
-    var testDirs = fs.readdirSync(testDir).filter(function(f) {
-      return fs.statSync(path.join(testDir, f)).isDirectory();
-    });
     var testTasks = [];
+    var testDirs = commands[opts.cmd] || [];
     testDirs.forEach(function (d) {
-        var cmd = 'dnx '+ opts.base + '/' + d + ' ' + opts.cmd;
-        var tname = 'test_' + d;
+        var cmd = 'dnx '+ d + ' ' + opts.cmd;
+        var tname = 'test_' + path.dirname(d);
         testTasks.push(tname);
         return gulp.task(tname, shell.task(cmd));
     });
     gulp.task('test', testTasks);
     gulp.task('test-watch', function () {
         testDirs.forEach(function (d) {
-            var tname = 'test_' + d;
+            var dir = path.dirname(d);
+            var tname = 'test_' + dir;
             var watchPaths = opts.watch.map(function (w) {
-                return opts.base + '/' + d + w;
+                return processPath(w, dir);
             });
             gulp.watch(watchPaths, [tname]);
         });
     });
+
+    return this;
 }
 
 function initWeb(gulp, opts) {
     opts = ext({}, opts, defaults.web);
 
+    if(!opts.base){
+        var webProjs = commands[opts.cmd] || [];
+        if(webProjs.length === 0) throw 'There are no projects with a web command.';
+        if(webProjs.length > 1) throw 'There are more than one projects with web commands. Please specify base command.';
+
+        opts.base = webProjs[0];
+    }
     if(!opts.base) throw '`base` is missing from web options.';
 
     var webProcess;
@@ -64,11 +91,13 @@ function initWeb(gulp, opts) {
     gulp.task('web', restartWeb);
     gulp.task('web-watch', function () {
         var watchPaths = opts.watch.map(function (w) {
-            return opts.base + w;
+            return processPath(w, opts.base);
         });
         gulp.watch(watchPaths, ['web']);
         restartWeb();
     });
+
+    return this;
 }
 
 module.exports.webTasks = initWeb;
